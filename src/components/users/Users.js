d8fe4c70a238f825ebf98f026f138ac81a5c720a
pm0824 <maneprachi824@gmail.com>
2021-05-22 19:43:35 +0530

    axios apigithub and spinner added

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,44 +1,21 @@
-import React, { Component } from 'react'
+import React from 'react'
 import UserItem from './UserItem'
+import Spinner from '../layout/Spinner'
 
-class Users extends Component {
-    state={
-        users:[
-            {
-                login: "mojombo",
-                id: '1',
-                avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-                html_url: "https://github.com/mojombo",
-            },
-            {
-                login: "defunkt",
-                id: '2',
-                avatar_url: "https://avatars.githubusercontent.com/u/2?v=4",
-                html_url: "https://github.com/defunkt",
-            },
-            {
-                login: "wycats",
-                id: '4',
-                avatar_url: "https://avatars.githubusercontent.com/u/4?v=4",
-                html_url: "https://github.com/wycats",
-            }
-        ]
+const Users =(props)=> {
+    if(props.loading){
+        return <Spinner/>
     }
-    render() {
+    else{
         return (
-            <div style={userStyle}>
+            <div className='grid-3'>
             {
-                this.state.users.map(user=>(
+              props.users.map(user=>(
                     <UserItem key={user.id} user={user}/>)
                 )
             }
             </div>
         )
     }
-}
-const userStyle={
-    display:'grid',
-    gridTemplateColumns:'repeat(3,1fr)',
-    gridGap:'1rem'
 }
 export default Users
